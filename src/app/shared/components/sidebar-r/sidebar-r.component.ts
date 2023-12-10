b6aa316397dc569553d6648240346e41720693b0
Mangesh <mangeshdhole97@gmail.com>
2023-12-10 10:28:30 +0530

    changes_10-12-23

---
+++ src/app/shared/components/sidebar-r/sidebar-r.component.ts
@@ -0,0 +1,58 @@
+import { AfterViewInit, Component, DoCheck, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-r',
+  templateUrl: './sidebar-r.component.html',
+  styleUrls: ['./sidebar-r.component.scss']
+})
+export class SidebarRComponent implements OnInit, OnChanges {
+
+
+  @Input() userObj!: any
+  remindersArray : any[] =[];
+  coursesArray : any[] =[];
+  @ViewChild('mySidenav') mySidenav!: ElementRef<HTMLElement>
+
+
+  constructor() { }
+
+
+  ngOnInit(): void {
+  
+    
+  }
+
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.remindersArray = this.userObj.reminders
+    // console.log(this.userObj.courses);
+
+    this.coursesArray = this.userObj.courses
+
+    let arr = []
+    for(let key in this.userObj.courses){
+      let obj = {
+        subject: key,
+        value: this.userObj.courses[key].slice(0,2)
+      }
+    
+      console.log(obj);
+      arr.push(obj)
+    }
+
+    this.coursesArray = arr
+    
+  }
+
+ 
+
+
+  openNav(mySidenav: HTMLElement){
+    mySidenav.classList.add('width-300')
+  }
+
+  closeNav(mySidenav: HTMLElement){
+    mySidenav.classList.remove('width-300')
+  }
+}
